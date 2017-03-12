7962998e65e95347faf80b1de5e3f444219fe1b8
okmich <okmich2002@yahoo.com>
2017-03-12 07:41:52 +0200

    after class

--- twitterstream-sent-app/src/main/scala/Main.scala
+++ twitterstream-sent-app/src/main/scala/Main.scala
@@ -13,6 +13,8 @@ import twitter4j.Status
 import twitter4j.conf._
 import twitter4j.auth.OAuthAuthorization
 
+import com.github.vspiewak.util.SentimentAnalysisUtils._
+
 object Main extends Serializable {
 
 	def main(args: Array[String]) : Unit = {
@@ -22,7 +24,7 @@ object Main extends Serializable {
 		}
 		val searchString = args mkString " "
 		val sparkConf = new SparkConf().setAppName("Twitter Streaming")
-		val sc = new StreamingContext(sparkConf, Seconds(30))
+		val sc = new StreamingContext(sparkConf, Seconds(5))
 
 		System.setProperty("twitter4j.oauth.consumerKey", "GRn40epjhYSrfmlSJH4rNemG8")
 		System.setProperty("twitter4j.oauth.consumerSecret", "Dx8GxpCkkUvp9FoYcXNNkNNYiRmHQjSDgfxoTs4m4JBVDxr4q1")
@@ -40,19 +42,22 @@ object Main extends Serializable {
 
 	def processTweetRDD(rdd: RDD[twitter4j.Status]) : Unit ={
 		//do cl
-		val resultRDD = rdd.filter(_.getLang == "en").map(_.getText).map((text: String) => {
-			//do actually classification and publishing of result
-			""
-		}).coalesce(1)
+		val results : scala.collection.Map[String, Long] = rdd.filter(_.getLang == "en").map(_.getText).map((txt: String) => {
+			detectSentiment(txt).toString
+		}).countByValue
 		//reduce resultRDD on count by classification
 
-		//sample data
-		import scala.util.Random
-		val rand = new Random
-		val results = Array(("POSITIVE", rand.nextInt(30)), 
-			("NEGATIVE",rand.nextInt(30)), 
-			("NEUTRAL", rand.nextInt(20))).map(a => {
-				a._1 + ":" + a._2.toString}).mkString("|")
-		redis.RedisProducer.publishResults(results)
+		// //sample data
+		// import scala.util.Random
+		// val rand = new Random
+		// val results = Array(("POSITIVE", rand.nextInt(30)), 
+		// 	("NEGATIVE",rand.nextInt(30)), 
+		// 	("NEUTRAL", rand.nextInt(20))).map(a => {
+		// 		a._1 + ":" + a._2.toString}).mkString("|")
+
+		val message = results.toArray.map((a:(String, Long)) => {
+		 		a._1 + ":" + a._2.toString}).mkString("|")
+		//send to message broker
+		redis.RedisProducer.publishResults(message)
 	}
 }
